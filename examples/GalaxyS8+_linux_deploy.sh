666fafa68b1e64cab971acf821c451b5f5299db8
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2017-09-20 00:52:41 +0900

    updated deploy script

--- examples/GalaxyS8+_linux_deploy.sh
+++ examples/GalaxyS8+_linux_deploy.sh
@@ -7,13 +7,41 @@
 # Install dependencies:
 DEPS="gnuradio swig pkgconf usbutils libuhd* libusb-dev"
 echo "Install $DEP"
-sudo apt-get install gnuradio swig pkgconf usbutils libuhd* libusb-dev
+sudo apt-get install git gnuradio swig pkgconf usbutils libuhd* libusb-dev
 
 # Setup udev rules for USRPs
 UHD_UDEV_LOC=usr/lib/uhd/utils
+
+echo "Setup udev rules for USRPs..."
 sudo cp $UHD_UDEV_LOC/uhd-usrp.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
+# Build gr-foo and gr-ieee802-15-4
+NTHREADS=8
+
+cd ~
+git clone https://github.com/dangnhat/gr-foo
+git clone https://github.com/dangnhat/gr-ieee802-15-4
+
+echo "Building gr-foo with $NTHREADS threads..."
+cd gr-foo
+mkdir build
+cd build
+cmake ../
+make -j$NTHREAD
+sudo make install
+sudo ldconfig
+
+cd ~
+echo "Building gr-ieee802-15-4 with $NTHREADS threads..."
+cd gr-ieee802-15-4
+mkdir build
+cd build
+cmake ../
+make -j$NTHREAD
+sudo make install
+sudo ldconfig
 
 # Run gnuradio companion to generate .py files. or use grcc:
 # Usage: grcc: [options] filename
