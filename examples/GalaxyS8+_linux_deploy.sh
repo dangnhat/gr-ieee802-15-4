ac2c526df30aea3e1acc3f4cd5de822e104cdaa8
Nhat Pham <phamhuudangnhat@gmail.com>
2017-09-19 21:41:57 +0900

    add script to deploy

---
+++ examples/GalaxyS8+_linux_deploy.sh
@@ -0,0 +1,19 @@
+# Deploy Ubuntu with > 4GB image file, grapic: VNC, 800x400, 100 dpi, pass: resl.
+
+# Install git and clone source code:
+# https://github.com/dangnhat/gr-foo
+# https://github.com/dangnhat/gr-ieee802-15-4
+
+# Install dependencies:
+DEPS="gnuradio swig pkgconf usbutils libuhd* libusb-dev"
+echo "Install $DEP"
+sudo apt-get install gnuradio swig pkgconf usbutils libuhd* libusb-dev
+
+# Setup udev rules for USRPs
+UHD_UDEV_LOC=usr/lib/uhd/utils
+sudo cp $UHD_UDEV_LOC/uhd-usrp.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+# Run gnuradio companion to generate .py files. or use grcc:
+# Usage: grcc: [options] filename
