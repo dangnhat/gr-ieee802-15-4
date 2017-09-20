3fccdff6cb8a76df07a5495b4152a90b5f7742b7
Nhat Pham <phamhuudangnhat@gmail.com>
2017-09-20 13:36:33 +0900

    updated deploy script

--- examples/GalaxyS8+_linux_deploy.sh
+++ examples/GalaxyS8+_linux_deploy.sh
@@ -6,12 +6,16 @@
 
 # Install dependencies:
 DEPS="gnuradio swig pkgconf usbutils libuhd* libusb-dev"
+echo
+echo
 echo "Install $DEP"
 sudo apt-get install git gnuradio swig pkgconf usbutils libuhd* libusb-dev cmake
 
 # Setup udev rules for USRPs
 UHD_UDEV_LOC=/usr/lib/uhd/utils
 
+echo
+echo
 echo "Setup udev rules for USRPs..."
 sudo cp $UHD_UDEV_LOC/uhd-usrp.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
@@ -20,10 +24,15 @@ sudo udevadm trigger
 # Build gr-foo and gr-ieee802-15-4
 NTHREADS=8
 
+echo
+echo
+echo "Cloning gr-foo and gr-ieee802-15-4"
 cd ~
 git clone https://github.com/dangnhat/gr-foo
 git clone https://github.com/dangnhat/gr-ieee802-15-4
 
+echo
+echo
 echo "Building gr-foo with $NTHREADS threads..."
 cd gr-foo
 mkdir build
@@ -34,6 +43,8 @@ sudo make install
 sudo ldconfig
 
 cd ~
+echo
+echo
 echo "Building gr-ieee802-15-4 with $NTHREADS threads..."
 cd gr-ieee802-15-4
 git checkout shcs_mac_demo
