a04e886cfe11183c5f507127abd63caa92d51307
Nhat Pham <phamhuudangnhat@gmail.com>
2017-10-20 16:36:09 +0900

    MAC: corrected dest and src pointers

--- lib/shcs_mac_impl.cc
+++ lib/shcs_mac_impl.cc
@@ -783,16 +783,8 @@ shcs_mac_impl::generate_mac (const uint8_t *buf, int len)
   }
 
   /* Peak to get src addr and dest addr */
-  /* Test */
-  cout << "MAC: TEST: buf: " << endl;
-  for (int count = 0; count < len; count++) {
-    cout << buf[count] << " ";
-  }
-  cout << endl;
-
-  const uint8_t* d_dest_addr = &buf[0];
-  if ((d_msg_len = ieee802154_set_frame_hdr (mhr, (uint8_t*) &d_mac_addr, 2,
-                                             (uint8_t*) &d_dest_addr, 2,
+  const uint8_t* dest_addr = &buf[0];
+  if ((d_msg_len = ieee802154_set_frame_hdr (mhr, d_mac_addr, 2, dest_addr, 2,
                                              pan_id_le, pan_id_le, flags,
                                              d_seq_nr++)) == 0) {
     dout << "MAC: header error." << endl;
