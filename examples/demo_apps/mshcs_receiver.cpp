efdcb66a2b1a4eeab8bb0ad50e2464b7d5bc370f
Nhat Pham <phamhuudangnhat@gmail.com>
2017-12-05 21:50:36 +0900

    added rtt test

---
+++ examples/demo_apps/mshcs_receiver.cpp
@@ -0,0 +1,102 @@
+//============================================================================
+// Name        : ngrc_suc_app.cpp
+// Author      : Nhat Pham
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <cstdio>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/* SUC UDP server info */
+int suc_udp_server_port = 52002, send_udp_port = 52001;
+const uint32_t buffer_len = 256;
+char buffer[buffer_len];
+
+int suc_socket_fd, send_socket_fd;
+
+void
+intHandler (int dummy)
+{
+  close (suc_socket_fd);
+  close (send_socket_fd);
+  exit (0);
+}
+
+int
+main ()
+{
+  unsigned int client_addr_len;
+  struct sockaddr_in my_addr, client_addr;
+  struct sockaddr_in send_addr;
+  int received_buf_len;
+
+  cout << "This is NGRC project SUC demo application" << endl;
+
+  /* Added signal handle for Ctrl-C */
+  signal (SIGINT, intHandler);
+
+  /* Create SUC UDP server socket */
+  if ((suc_socket_fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+    cout << "cannot create socket" << endl;
+    return 0;
+  }
+
+  memset ((char *) &my_addr, 0, sizeof(my_addr));
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+  my_addr.sin_port = htons (suc_udp_server_port);
+
+  if (bind (suc_socket_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)) < 0) {
+    cout << "bind failed" << endl;
+    return 0;
+  }
+
+  /* Wait for message from SUs and forward them to control center */
+  while (1) {
+    /* Waiting for messages */
+    received_buf_len = recvfrom (suc_socket_fd, (void *) buffer, buffer_len, 0,
+                                 (sockaddr *) &client_addr, &client_addr_len);
+    if (received_buf_len == -1) {
+      cout << "recvfrom error." << endl;
+      continue;
+    }
+
+    buffer[received_buf_len] = '\0';
+    cout << "\nReceived: " << buffer << endl;
+
+    /* Echo back to client */
+    if ((send_socket_fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+      cout << "cannot create socket" << endl;
+      return 0;
+    }
+
+    memset ((char *) &send_addr, 0, sizeof(send_addr));
+    send_addr.sin_family = AF_INET;
+    send_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+    send_addr.sin_port = htons (send_udp_port);
+
+    if (bind (send_socket_fd, (struct sockaddr *) &send_addr, sizeof(send_addr))
+        < 0) {
+      cout << "bind failed" << endl;
+      return 0;
+    }
+
+    sendto (send_socket_fd, (void *) buffer, received_buf_len, 0,
+            (sockaddr *) &client_addr, client_addr_len);
+    close (send_socket_fd);
+  }
+
+  return 0;
+}
+
---
+++ examples/demo_apps/mshcs_sender.cpp
@@ -0,0 +1,158 @@
+//============================================================================
+// Name        : ngrc_su_app.cpp
+// Author      : Nhat Pham
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <cstdio>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sstream>
+#include <boost/chrono.hpp>
+#include <signal.h>
+
+using namespace std;
+using namespace boost::chrono;
+
+/* UDP server info */
+string addr = "127.0.0.1";
+int send_port = 52001, recv_port = 52002;
+int send_socket_fd, recv_socket_fd;
+
+const string recv_addr = "12.34";
+const string send_addr = "12.35";
+const int wait_time_range[2] = { 2, 10 };
+//const string message = "Hello World.\n";
+
+void
+intHandler (int dummy)
+{
+  close (recv_socket_fd);
+  close (send_socket_fd);
+  exit (0);
+}
+
+int
+main ()
+{
+  struct sockaddr_in myaddr;
+  struct sockaddr_in dest_addr, recv_addr;
+  unsigned int recv_addr_len;
+  const uint32_t ack_buffer_len = 256;
+  char ack_buffer[ack_buffer_len];
+  int ack_recv_buffer_len;
+
+  ostringstream message;
+  uint64_t seqno = 0, recv_seqno = 0;
+  uint64_t numAckedPackets = 0;
+  system_clock::time_point send_time, ack_time;
+  int rand_wait_time;
+
+  cout << "This is MSHCS round trip delay test - sender side." << endl;
+
+  /* Added signal handle for Ctrl-C */
+  signal (SIGINT, intHandler);
+
+  /* Create receiving socket */
+  if ((recv_socket_fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+    cout << "cannot create recv_socket" << endl;
+    return 0;
+  }
+
+  memset ((char *) &myaddr, 0, sizeof(myaddr));
+  myaddr.sin_family = AF_INET;
+  myaddr.sin_addr.s_addr = htonl (INADDR_ANY);
+  myaddr.sin_port = htons (recv_port);
+
+  if (bind (recv_socket_fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
+    cout << "bind failed" << endl;
+    return 0;
+  }
+
+  /* Set timeout for socket 3s */
+  struct timeval tv;
+  tv.tv_sec = 3;
+  tv.tv_usec = 0;
+  if (setsockopt (recv_socket_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv))
+      < 0) {
+    cout << "Set timeout error" << endl;
+  }
+
+  while (1) {
+    /* Generate su_status message */
+    message.clear ();
+    message.str ("");
+    message << recv_addr << " " << send_addr << " " << seqno << endl;
+    cout << "Message: " << message.str () << endl;
+
+    /* Create a socket */
+    if ((send_socket_fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+      cout << "cannot create socket" << endl;
+      return 0;
+    }
+
+    memset ((char *) &myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl (INADDR_ANY);
+    myaddr.sin_port = htons (0);
+
+    if (bind (send_socket_fd, (struct sockaddr *) &myaddr, sizeof(myaddr))
+        < 0) {
+      cout << "bind failed" << endl;
+      return 0;
+    }
+
+    /* periodically send status to Coordinator */
+    dest_addr.sin_family = AF_INET;
+    inet_aton (addr.c_str (), &dest_addr.sin_addr);
+    dest_addr.sin_port = htons (send_port);
+
+    /* Wait to receive ACK for a random time */
+    rand_wait_time = rand () % (wait_time_range[1] - wait_time_range[0] + 1)
+        + wait_time_range[0];
+    cout << "Random wait time: " << rand_wait_time << endl;
+    sleep (rand_wait_time);
+
+    sendto (send_socket_fd, message.str ().c_str (), message.str ().length (),
+            0, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
+    close (send_socket_fd);
+
+    /* Get current time */
+    send_time = system_clock::now ();
+
+    /* Wait for ack */
+    ack_recv_buffer_len = recvfrom (recv_socket_fd, ack_buffer, ack_buffer_len,
+                                    0, (struct sockaddr *) &recv_addr,
+                                    &recv_addr_len);
+    if (ack_recv_buffer_len < 0) {
+      cout << "Recvfrom error or timeout" << endl;
+    }
+    else {
+      /* Get current time */
+      ack_time = system_clock ();
+
+      ack_buffer[ack_recv_buffer_len] = 0;
+      cout << "Received: " << ack_buffer << endl;
+
+      string ack_string (ack_buffer);
+      ack_string >> recv_seqno;
+      if (recv_seqno == seqno) {
+        numAckedPackets++;
+        cout << "ACKed for #" << seqno << ", acked: " << numAckedPackets
+            << ", RTT: " << ack_time - send_time << endl;
+      }
+    }
+
+    seqno++;
+  }
+
+  return 0;
+}
+
